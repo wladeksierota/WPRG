b76f060226be7c724ecf54b8f89259a913df583a
wladeksierota <wladek.sierota@gmail.com>
2025-06-09 17:30:40 +0200

    zad1.php

--- 9/zad1/zad1.php
+++ 9/zad1/zad1.php
@@ -6,35 +6,25 @@
 </head>
 <body>
 <form method="get" action="zad1.php">
-    <label for="birthdate">Wprowadź swoją datę urodzenia:</label>
+    <label for="birthdate">Podaj date urodzenia:</label>
     <input type="date" name="birthdate" required>
-    <input type="submit" value="Wyślij">
+    <input type="submit" value="wyślij">
 </form>
 <?php
 if (isset($_GET['birthdate'])) {
     $birthdate = $_GET['birthdate'];
     $birthDateObj = new DateTime($birthdate);
     $today = new DateTime();
-    $dayOfWeek = $birthDateObj->format('l'); // np. Monday
-    $daysPolish = [
-        'Monday' => 'Poniedziałek',
-        'Tuesday' => 'Wtorek',
-        'Wednesday' => 'Środa',
-        'Thursday' => 'Czwartek',
-        'Friday' => 'Piątek',
-        'Saturday' => 'Sobota',
-        'Sunday' => 'Niedziela'
-    ];
-    // 2. Wiek użytkownika
+    $dayOfWeek = $birthDateObj->format('l');
     $age = $birthDateObj->diff($today)->y;
-    // 3. Ilość dni do najbliższych urodzin
+
     $nextBirthday = DateTime::createFromFormat('Y-m-d', $today->format('Y') . '-' . $birthDateObj->format('m-d'));
     if ($nextBirthday < $today) {
         $nextBirthday->modify('+1 year');
     }
     $daysToBirthday = $today->diff($nextBirthday)->days;
     echo "<h3>Wyniki:</h3>";
-    echo "<p>Urodziłeś się w: <strong>" . $daysPolish[$dayOfWeek] . "</strong></p>";
+    echo "<p>Urodziłeś się w: <strong>" .$dayOfWeek. "</strong></p>";
     echo "<p>Masz: <strong>$age</strong> lat</p>";
     echo "<p>Do najbliższych urodzin zostało: $daysToBirthday</p>";
 }
